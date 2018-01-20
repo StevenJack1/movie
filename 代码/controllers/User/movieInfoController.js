4e385671f3fc86e5dbe006ccdb3bdaf30668a019
mjspecial <13035319028@139.com>
2018-01-20 10:53:53 +0800

    电影详情-部分

--- 代码/controllers/User/movieInfoController.js
+++ 代码/controllers/User/movieInfoController.js
@@ -1,19 +1,42 @@
 var { sequelize,Sequelize} = require('../../config/db');
-// var User = sequelize.import('../../models/User');
+var Movie = sequelize.import('../../models/Movie');
 
 module.exports = {
 
-    getInfo: function (req,res,next) {                      // 查
-        res.render('User/movieInfo',{
-            title: '放牛班的春天',
-            original_title: 'Les choristes',
-            year: '2004',
-            img: 'https://img3.doubanio.com/view/movie_poster_cover/spst/public/p1910824951.jpg',
-            directors: '\t克里斯托夫·巴拉蒂',
-            casts: '\t热拉尔·朱尼奥,让-巴蒂斯特·莫尼耶,弗朗索瓦·贝莱昂',
-            genres: '\t剧情 / 音乐',
-            alt: '\thttps://movie.douban.com/subject/1291549/',
-            rating: '9.2',
-        });
-    }
+    getInfo: function (req,res,next) {                                      // 查
+        Movie.findById('1').then(function (result) {
+            res.render('User/movieInfo',{
+                id: result.id,
+                title: result.title,
+                original_title: result.original_title,
+                year: result.year,
+                img: result.image,
+                directors: result.directors,
+                casts: result.casts,
+                genres: result.genres,
+                alt: result.alt,
+                rating: result.rating,
+            })
+        }).catch(next)
+    },
+
+//     createInfo:function (req, res, next) {
+//         Movie.create( {
+//             id: '1',
+//             title: '放牛班的春天',
+//             original_title: 'Les choristes',
+//             year: '2004',
+//             img: 'https://img3.doubanio.com/view/movie_poster_cover/spst/public/p1910824951.jpg',
+//             directors: '克里斯托夫·巴拉蒂',
+//             casts: '热拉尔·朱尼奥,让-巴蒂斯特·莫尼耶,弗朗索瓦·贝莱昂',
+//             genres: '剧情 / 音乐',
+//             alt: 'https://movie.douban.com/subject/1291549/',
+//             rating: '9.2',
+//         }).then(function (result) {
+//             res.json({
+//                 status: 1,
+//                 data: result,
+//             });
+//         }).catch(next);
+//     },
 };
