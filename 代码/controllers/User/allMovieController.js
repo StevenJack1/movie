3a416897c2c8edc01e2bad8aaba8da0e4c19cce8
linbaishuang <1765259718@qq.com>
2018-01-21 11:18:19 +0800

    实现分类搜索

--- 代码/controllers/User/allMovieController.js
+++ 代码/controllers/User/allMovieController.js
@@ -1,10 +1,13 @@
+
 var { sequelize,Sequelize} = require('../../config/db');
 var User = sequelize.import('../../models/User');
 var Movie = sequelize.import('../../models/Movie');
 var _       = require('lodash');            //_.pick导入
 var marked = require('marked');             //_.pick导入
+var mysql = require('mysql');
 // app.use(bodyParser.urlencoded())
 
+// connection.connect();
 module.exports = {
 
     getInfo: function (req,res,next) {                      // 查
@@ -15,34 +18,11 @@ module.exports = {
         if(req.method === 'POST' ){
             var clickName = _.pick(req.body, 'className');
             var className = clickName.className;
-
-            Movie.findAll({
-                // genres: /"+className+"/
-            }).then( function (result) {
-                // console.log(result);
-                var data = [];
-                // result.forEach(function (result) {
-                //     var movieData = {
-                //         id:result.id,
-                //         alt:result.alt,
-                //         year:result.year,
-                //         title:result.title,
-                //         original_title:result.original_title,
-                //         rating:result.rating,
-                //         directors:result.directors,
-                //         casts:result.casts,
-                //         genres:result.genres,
-                //         image:result.image,
-                //         description:result.description
-                //     }
-                //     // console.log(movieData);
-                //     data.push(movieData);
-                //     len = data.length;
-                // });
-                // console.log(data);
-                res.send(data);
-            }).catch(function (err) {
-                console.log(err);
+            console.log(className);
+            var sql = "select * from Movie where genres like '%"+className+"%'; ";
+            sequelize.query(sql).spread((result, metadata) => {
+                console.log(result);
+                res.send(result);
             });
         } else if(req.method === 'GET'){
             Movie.findAll().then(function (result) {
