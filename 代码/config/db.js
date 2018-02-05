52a1b6c495e8e57fd801c470dbea9af30b847827
严泽凡 <18231550929@sina.com>
2018-02-05 16:15:18 +0800

    去掉sql打印数据到控制台

--- 代码/config/db.js
+++ 代码/config/db.js
@@ -10,12 +10,13 @@ const sequelize = new Sequelize('movie','root','root',{
         max:5,
         min:0,
         idle:10000
-    }
+    },
+	logging: false
 });
 
 //测试数据库链接
 sequelize.authenticate().then(function() {
-    console.log("数据库连接成功");
+    // console.log("数据库连接成功");
 }).catch(function(err) {
     //数据库连接失败时打印输出
     console.error(err);
